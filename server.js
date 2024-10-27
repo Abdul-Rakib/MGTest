72ad6a8dd01a3dc74d1355d68ca40deb30e09958
abdulrakib <firefalls2004@gmail.com>
2024-10-27 22:43:25 +0530

    removed pin

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ async function getPublicIp() {
         return 'Could not retrieve public IP';
     }
 }
-
 // Log the public IP address
 getPublicIp().then(ip => {
     console.log(`Server's public IP address: ${ip}`);
@@ -24,11 +23,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post('/proxy-test', async (req, res) => {
     const apiKey = 'API9NTAZM1714702501999';
-    const pin = '123456'; // Use your PIN here
+    // const pin = '123456'; // Use your PIN here
 
     try {
         const response = await axios.post('https://a-api.yokcash.com/api/service', 
-            new URLSearchParams({ api_key: apiKey, pin: pin }), 
+            // new URLSearchParams({ api_key: apiKey, pin: pin }), 
+            new URLSearchParams({ api_key: apiKey}), 
             {
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
