8a1dc42cc5ef32053129c01654d69f84d3ede7bf
abdulrakib <firefalls2004@gmail.com>
2024-12-13 17:18:01 +0530

    try

--- server.js
+++ server.js
@@ -6,16 +6,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Partner ID and Secret Key
 const partnerId = '0b1301175ee58047e36446facff8e57d';
 const secretKey = 'dw5KQpUMIk';
 
-// Function to generate Basic Auth
 const generateBasicAuth = (partnerId, secretKey) => {
   return Buffer.from(`${partnerId}:${secretKey}`).toString('base64');
 };
 
-// Function to generate Auth Signature
 const generateAuthSignature = (payload, timestamp, path, secretKey) => {
   const stringToSign = `${payload}${timestamp}${path}`;
   return crypto.createHmac('sha256', secretKey).update(stringToSign).digest('hex');
@@ -61,6 +58,84 @@ app.post('/test', async (req, res) => {
   }
 
 });
+
+// API route to create order
+app.post('/order/create_order', async (req, res) => {
+  try {
+
+    const { path, ...otherParams } = req.body;
+
+    const payload = JSON.stringify({
+      path: path,
+      ...otherParams,
+    });
+
+    const timestamp = Math.floor(Date.now() / 1000);
+    const basicAuth = generateBasicAuth(partnerId, secretKey);
+
+    const authSignature = generateAuthSignature(payload, timestamp, path, secretKey);
+
+    const response = await axios.post(
+      `https://moogold.com/wp-json/v1/api/order/create_order`,
+      payload,
+      {
+        headers: {
+          Authorization: `Basic ${basicAuth}`,
+          auth: authSignature,
+          timestamp: timestamp.toString(),
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({
+      error: true,
+      message: error.message,
+      details: error.response?.data || 'Internal Server Error',
+    });
+  }
+});
+
+app.post('/order/order_detail_partner_id', async (req, res) => {
+  try {
+
+    const { path, ...otherParams } = req.body;
+
+    const payload = JSON.stringify({
+      path: path,
+      ...otherParams,
+    });
+
+    const timestamp = Math.floor(Date.now() / 1000);
+    const basicAuth = generateBasicAuth(partnerId, secretKey);
+
+    const authSignature = generateAuthSignature(payload, timestamp, path, secretKey);
+
+    const response = await axios.post(
+      `https://moogold.com/wp-json/v1/api/order/order_detail_partner_id`,
+      payload,
+      {
+        headers: {
+          Authorization: `Basic ${basicAuth}`,
+          auth: authSignature,
+          timestamp: timestamp.toString(),
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({
+      error: true,
+      message: error.message,
+      details: error.response?.data || 'Internal Server Error',
+    });
+  }
+});
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
